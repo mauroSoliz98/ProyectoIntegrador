eecfc7d7a81dea3886f1bfd24318c1bc01959de3
mauroSoliz98 <maurososoliz@gmail.com>
2025-03-25 17:50:18 -0400

    Se agrego el drawer de inundacion

--- src/Pages/Map/Map.jsx
+++ src/Pages/Map/Map.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
 import L from 'leaflet';
-import { Drawer } from 'antd';
-import { UserOutlined,TeamOutlined } from '@ant-design/icons';
+import { Drawer, Card, Avatar, List, Typography } from 'antd';
+import { MdLocationOn } from "react-icons/md";
 import floodIcon from '../../assets/flood.png';
 import landslideIcon from '../../assets/landslide.png';
+const { Meta } = Card;
+const { Text } = Typography;
 
 // Coordenadas de Cochabamba
 const COCHABAMBA_COORDS = [-17.3895, -66.1568];
@@ -24,8 +26,23 @@ const iconoDeslizamiento = new L.Icon({
   popupAnchor: [0, -38],
 });
 
+
 const Map = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const data = [
+    {
+      title: 'Ant Design Title 1',
+    },
+    {
+      title: 'Ant Design Title 2',
+    },
+    {
+      title: 'Ant Design Title 3',
+    },
+    {
+      title: 'Ant Design Title 4',
+    },
+  ];
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -40,17 +57,33 @@ const Map = () => {
 
   return (
     <>
-     <Drawer
-        title="Basic Drawer"
+      <Drawer
         placement={'right'}
         closable={false}
         onClose={handleCancel}
         open={isModalOpen}
         width="50%"
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <Card cover={<div className="bg-[url(https://cdn.leonardo.ai/users/11fb33c1-1772-448f-bd6b-16e3e19e3b08/generations/ca678d30-7176-40d5-9c2c-a1e6eeebf94c/Leonardo_Phoenix_10_A_colorful_flat_design_illustration_of_the_2.jpg)] w-full h-40 bg-center" />}>
+          <Meta
+            title={<span className="flex items-center gap-1"><MdLocationOn /> Localización</span>}
+            description="C/ España y Mexico"
+          />
+        </Card>
+        <List
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={(item, index) => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+                title={<a href="https://ant.design">{item.title}</a>}
+                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+              />
+            </List.Item>
+          )}
+        />
+
       </Drawer>
 
       <MapContainer
@@ -65,7 +98,7 @@ const Map = () => {
         />
         {/* Marcador de inundación */}
         <Circle center={[-17.3895, -66.1568]} radius={500} color="blue">
-          <Marker position={[-17.3895, -66.1568]} icon={iconoInundacion} eventHandlers={{ click: showModal}}>
+          <Marker position={[-17.3895, -66.1568]} icon={iconoInundacion} eventHandlers={{ click: showModal }}>
             <Popup>¡Inundación reportada aquí!</Popup>
           </Marker>
         </Circle>
