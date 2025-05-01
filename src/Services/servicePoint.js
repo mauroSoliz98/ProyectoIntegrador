4a70fca7bd62d33d64fa0fa01bbbf497fda2ee56
mauroSoliz98 <maurososoliz@gmail.com>
2025-05-01 11:19:27 -0400

    Preparado para ser subido a render

--- src/Services/servicePoint.js
+++ src/Services/servicePoint.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:8000/api/points";
+const baseURL = "/api/points";
 
 const getAll = async() => {
     try {
--- src/Services/serviceRegister.js
+++ src/Services/serviceRegister.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:8000/api/auth";
+const baseURL = "/api/auth";
 
 const create = async (data) => {
   try {
